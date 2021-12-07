ccd5db6087c5e9ade8c757eebcda98f467a7cbb8
Ian Nkwocha <11nkwochaian@gmail.com>
2021-12-07 13:07:29 +0000

    l

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -1,4 +1,5 @@
 class GildedRose(object):
+    MAX_QUALITY = 50
 
     def __init__(self, items):
         self.items = items
@@ -7,27 +8,27 @@ class GildedRose(object):
         BACKSTAGE_PASS_STRING = "Backstage passes to a TAFKAL80ETC concert"
         SULFURAS_STRING = "Sulfuras, Hand of Ragnaros"
         AGED_BRIE_STRING = "Aged Brie"
-        MAX_QUALITY = 50
+        
         for item in self.items:
             if item.name != AGED_BRIE_STRING and item.name != BACKSTAGE_PASS_STRING:
                 if item.quality > 0:
                     if item.name != SULFURAS_STRING:
                         self.decrement_quality(item)
             else:
-                if item.quality < MAX_QUALITY:
+                if item.quality < self.MAX_QUALITY:
                     self.increment_quality(item)
                     if item.name == BACKSTAGE_PASS_STRING:
                         if item.sell_in < 11:
-                            if item.quality < MAX_QUALITY:
+                            if item.quality < self.MAX_QUALITY:
                                 self.increment_quality(item)
                         if item.sell_in < 6:
-                            if item.quality < MAX_QUALITY:
+                            if item.quality < self.MAX_QUALITY:
                                 self.increment_quality(item)
             if item.name != SULFURAS_STRING:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name == AGED_BRIE_STRING:
-                    if item.quality < MAX_QUALITY:
+                    if item.quality < self.MAX_QUALITY:
                         self.increment_quality(item)
                 else:
                     if item.name != BACKSTAGE_PASS_STRING:
@@ -42,5 +43,7 @@ class GildedRose(object):
 
     def increment_quality(self, item):
         item.quality += 1
+        if item.quality > self.MAX_QUALITY:
+            item.quality = self.MAX_QUALITY
 
     
