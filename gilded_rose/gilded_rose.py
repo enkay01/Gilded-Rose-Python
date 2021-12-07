bee788d92391bc6884efbdfc9f1a435b412c8873
Ian Nkwocha <11nkwochaian@gmail.com>
2021-12-07 11:46:03 +0000

    Replaced hard coded strings

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -4,31 +4,35 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
+        BACKSTAGE_PASS_STRING = "Backstage passes to a TAFKAL80ETC concert"
+        SULFURAS_STRING = "Sulfuras, Hand of Ragnaros"
+        AGED_BRIE_STRING = "Aged Brie"
+        MAX_QUALITY = 50
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != AGED_BRIE_STRING and item.name != BACKSTAGE_PASS_STRING:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != SULFURAS_STRING:
                         item.quality = item.quality - 1
             else:
-                if item.quality < 50:
+                if item.quality < MAX_QUALITY:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == BACKSTAGE_PASS_STRING:
                         if item.sell_in < 11:
-                            if item.quality < 50:
+                            if item.quality < MAX_QUALITY:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
+                            if item.quality < MAX_QUALITY:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != SULFURAS_STRING:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name == AGED_BRIE_STRING:
+                    if item.quality < MAX_QUALITY:
+                        item.quality = item.quality + 1
+                else:
+                    if item.name != BACKSTAGE_PASS_STRING:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != SULFURAS_STRING:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
